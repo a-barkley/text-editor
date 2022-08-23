a24ce87031f7f23f9e5b91aaa92e343c199cd802
pbarkley_desk <rpbarkley99@gmail.com>
2022-08-22 20:20:41 -0500

    update webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,7 +22,10 @@ module.exports = () => {
         template: './index.html',
         title: 'JATE'
       }),
-      new InjectManifest(),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
       new WebpackPwaManifest({
         name: 'Just Another Text Editor',
         short_name: 'JATE',
