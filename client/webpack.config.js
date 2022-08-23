14daab864cc88c1dbcb7ddb6a22571bba055bfda
pbarkley_desk <rpbarkley99@gmail.com>
2022-08-22 19:34:38 -0500

    add rules to config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,14 +23,28 @@ module.exports = () => {
         title: 'JATE'
       }),
       new InjectManifest(),
-      new WebpackPwaManifest(P{
-        
+      new WebpackPwaManifest({
+
       })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
